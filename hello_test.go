f7c72932248bc893370eb678329201c71e5429e4
Jim Shaw <jimxshaw@gmail.com>
2022-03-28 22:04:23 -0500

    Refactor TestHello to use help function.

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("James")
-	want := "Hello James!"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("Greeting users", func(t *testing.T) {
+		got := Hello("James")
+		want := "Hello James!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Greet with 'Hello Go!' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello Go!"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
