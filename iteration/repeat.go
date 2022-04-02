1e1677a5dd8e636ef85368527af9031a8c9ea3b7
Jim Shaw <jimxshaw@gmail.com>
2022-04-01 21:13:27 -0500

    Refactor Repeat function.

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,11 @@
 package iteration
 
+const repeatCount = 4
+
 func Repeat(character string) string {
 	var repeated string
-	for i := 0; i < 4; i++ {
-		repeated = repeated + character
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
 	}
 
 	return repeated
