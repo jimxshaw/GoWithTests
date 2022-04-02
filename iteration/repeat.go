6107c38c1d4df6eb25694bea2776981fd1efb2fb
Jim Shaw <jimxshaw@gmail.com>
2022-04-01 22:39:53 -0500

    Update Repeat function with conditions and test cases.

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,6 +1,10 @@
 package iteration
 
 func Repeat(character string, howManyTimes int) string {
+	if howManyTimes <= 0 || howManyTimes > 1000 {
+		return "howManyTimes can only be between 1 and 1000"
+	}
+
 	var repeated string
 	for i := 0; i < howManyTimes; i++ {
 		repeated += character
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,18 +1,40 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("y", 3)
-	expected := "yyy"
-
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+	assertCorrectMessage := func(t testing.TB, expected, repeated string) {
+		t.Helper()
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
 	}
+
+	t.Run("Repeat a specified number of times", func(t *testing.T) {
+		repeated := Repeat("y", 3)
+		expected := "yyy"
+		assertCorrectMessage(t, expected, repeated)
+	})
+
+	t.Run("Prevent repeat if invalid input", func(t *testing.T) {
+		repeated := Repeat("y", 0)
+		expected := "howManyTimes can only be between 1 and 1000"
+		assertCorrectMessage(t, expected, repeated)
+	})
+
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("y", 2)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 7)
+	fmt.Println(repeated)
+	// Output: aaaaaaa
 }
