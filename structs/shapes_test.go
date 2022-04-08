1d6d3502e785528fc69b08ac4b3f36c5eb2b1c04
Jim Shaw <jimxshaw@gmail.com>
2022-04-08 13:47:45 -0500

    Implement Perimeter function and update TestPerimeter.

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -1,17 +1,14 @@
 package structs
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
 func TestPerimeter(t *testing.T) {
 	got := Perimeter(2.0, 4.0)
-	expected := 20.0
+	expected := 12.0
 
 	if got != expected {
 		// The new f format string is for
 		// float64 to 2 decimal places.
-		fmt.Errorf("expected %.2f but got %.2f", expected, got)
+		t.Errorf("expected %.2f but got %.2f", expected, got)
 	}
 }
