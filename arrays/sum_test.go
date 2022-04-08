309094c25b4664f6d5af0b53dce767f8a6275310
Jim Shaw <jimxshaw@gmail.com>
2022-04-07 21:34:21 -0500

    Add test for slice, update Sum function.

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,22 +3,28 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	assertCorrectMessage := func(t testing.TB, got, expected int, given [5]int) {
-		t.Helper()
-		if got != expected {
-			t.Errorf("expected %d but got %d given %v", expected, got, given)
-		}
-	}
-
-	t.Run("Sum all ints in an array", func(t *testing.T) {
+	t.Run("Collection of size 5", func(t *testing.T) {
 		// Arrays have a fixed capacity that is
 		// defined when they are declared.
-		numbers := [5]int{2, 4, 6, 8, 10}
+		numbers := []int{2, 4, 6, 8, 10}
 
 		got := Sum(numbers)
 		expected := 30
 
-		assertCorrectMessage(t, got, expected, numbers)
+		if got != expected {
+			t.Errorf("expected %d but got %d given %v", expected, got, expected)
+		}
+	})
+
+	t.Run("Collection of any size", func(t *testing.T) {
+		numbers := []int{1, 3, 5}
+
+		got := Sum(numbers)
+		expected := 9
+
+		if got != expected {
+			t.Errorf("expected %d but got %d given %v", expected, got, expected)
+		}
 	})
 
 }
