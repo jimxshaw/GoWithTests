ef05a3d98bc2c0c7c1c18637985cf7c4214453c8
Jim Shaw <jimxshaw@gmail.com>
2022-04-08 15:29:55 -0500

    Refactor TestArea to use Table Driven Testing.

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -15,27 +15,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, expected float64) {
-		t.Helper()
-		got := shape.Area()
-
-		if got != expected {
-			// The g format string will print more
-			// precise decimal number in the message.
-			t.Errorf("expected %g but got %g", expected, got)
-		}
+	areaTests := []struct {
+		Shape    Shape
+		Expected float64
+	}{
+		{Rectangle{2.0, 4.0}, 8.0},
+		{Circle{10.0}, 314.1592653589793},
 	}
 
-	t.Run("Rectangles", func(t *testing.T) {
-		rectangle := Rectangle{2.0, 4.0}
-		expected := 8.0
-		checkArea(t, rectangle, expected)
-	})
+	// This is an example of Table Driven Testing:
+	// https://github.com/golang/go/wiki/TableDrivenTests
+	for _, tableTest := range areaTests {
+		got := tableTest.Shape.Area()
 
-	t.Run("Circles", func(t *testing.T) {
-		circle := Circle{10.0}
-		expected := 314.1592653589793
-		checkArea(t, circle, expected)
-	})
-
+		if got != tableTest.Expected {
+			// The g format string will print more
+			// precise decimal number in the message.
+			t.Errorf("expected %g but got %g", tableTest.Expected, got)
+		}
+	}
 }
