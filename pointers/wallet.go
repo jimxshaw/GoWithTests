806b050135bb1fbb9058cf9e8367de273816776e
Jim Shaw <jimxshaw@gmail.com>
2022-04-11 20:59:11 -0500

    Refactor wallet methods to receive pointers.

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -6,11 +6,13 @@ type Wallet struct {
 	balance int
 }
 
-func (w Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount int) {
 	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() int {
+	// (*w).balance is also valid syntax but explicit dereferencing
+	// isn't necessary as struct pointers are automatically dereferenced.
 	return w.balance
 }
