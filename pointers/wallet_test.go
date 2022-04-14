2b2c9997f7fc54d4733a149787bcc5255184cc7d
Jim Shaw <jimxshaw@gmail.com>
2022-04-13 22:07:40 -0500

    Add boilerplate Withdraw test.

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -6,18 +6,34 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(20))
+		wallet.Deposit(Bitcoin(20))
 
-	got := wallet.Balance()
+		got := wallet.Balance()
 
-	fmt.Printf("address of balance in test %v \n", &wallet.balance)
+		fmt.Printf("address of balance in test %v \n", &wallet.balance)
 
-	expected := Bitcoin(20)
+		expected := Bitcoin(20)
 
-	if got != expected {
-		t.Errorf("expected %s but got %s", expected, got)
-	}
+		if got != expected {
+			t.Errorf("expected %s but got %s", expected, got)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(150)}
+
+		wallet.Withdraw(Bitcoin(100))
+
+		got := wallet.Balance()
+
+		expected := Bitcoin(50)
+
+		if got != expected {
+			t.Errorf("expected %s but got %s", expected, got)
+		}
+	})
 
 }
