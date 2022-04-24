41b5b051a49c21138614707a01cfcd073422ae35
Jim Shaw <jimxshaw@gmail.com>
2022-04-24 12:29:02 -0500

    Create new DictionaryErr type that implements error interface.

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,13 +1,21 @@
 package maps
 
-import "errors"
+const (
+	ErrNotFound   = DictionaryErr("search term cannot be found")
+	ErrTermExists = DictionaryErr("cannot add term because it already exists")
+)
+
+// Create this custom type that implements the
+// error interface to make errors more
+// reusable and immutable.
+// https://dave.cheney.net/2016/04/07/constant-errors
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 type Dictionary map[string]string
-
-var (
-	ErrNotFound   = errors.New("search term cannot be found")
-	ErrTermExists = errors.New("cannot add term because it already exists")
-)
 
 func (d Dictionary) Search(term string) (string, error) {
 	definition, ok := d[term]
