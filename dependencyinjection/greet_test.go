62001c47d281c67aafde84d0d78449224d7e5bd8
Jim Shaw <jimxshaw@gmail.com>
2023-06-29 17:10:47 -0500

    Add Greet and test.

---
+++ dependencyinjection/greet_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Joe")
+
+	got := buffer.String()
+	want := "Hello, Joe"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
